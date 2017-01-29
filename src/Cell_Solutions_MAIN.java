af106167a77c5130d9c5ceeb128fa0dd2b2896b4
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2017-01-29 11:02:54 -0600

    Commit.

--- src/Cell_Solutions_MAIN.java
+++ src/Cell_Solutions_MAIN.java
@@ -4,10 +4,19 @@ Joshua Pickenpaugh
 Unit 3 Assignments: Cell Solutions (MAIN CLASS).
  */
 
-/**
- *
- * @author Joshua
- */
+import javax.swing.*;
+ 
 public class Cell_Solutions_MAIN {
     
+        //Main entry point for program:
+        public static void main(String args[])
+        {
+            Cell_Solutions cellSolutions = new Cell_Solutions();
+            cellSolutions.setTitle("Cell Solutions");
+            //Centers the window on start:
+            cellSolutions.setLocationRelativeTo(null);
+            cellSolutions.setVisible(true);
+            cellSolutions.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        }
+        
 }
